26d0e5c8de3a7eff2b1043fcbb260d3b1dad2081
antonio mauricio <antonioimportant@gmail.com>
2024-12-20 09:51:19 -0300

    feat: adds more routes into main navbar

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -5,6 +5,9 @@ import { RouterLink } from "vue-router";
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink> -
+    <RouterLink to="/today-diary">Today's Diary</RouterLink> -
+    <RouterLink to="/diary-pages">Diary Pages</RouterLink> -
+    <RouterLink to="/read-diary">Read Diary</RouterLink> -
     <RouterLink to="/config">Config</RouterLink>
   </nav>
 </template>
