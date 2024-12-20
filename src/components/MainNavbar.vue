a2fb6785176cc428423b8781b06877263116a4a1
antonio mauricio <antonioimportant@gmail.com>
2024-12-20 09:48:35 -0300

    feat: adds main navbar component

---
+++ src/components/MainNavbar.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> -
+    <RouterLink to="/config">Config</RouterLink>
+  </nav>
+</template>
+
+<style scoped></style>
