c03977b13e23d52b63807af533f64169dee11ce1
antonio mauricio <antonioimportant@gmail.com>
2024-12-20 08:53:47 -0300

    feat: ensures users cannot access home page if the userData is not set

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "./pages/HomePage.vue";
+import ConfigPage from "./pages/ConfigPage.vue";
 import NotFoundPage from "./pages/NotFoundPage.vue";
 
 const routes = [
@@ -7,6 +8,17 @@ const routes = [
     path: "/",
     name: "home",
     component: HomePage,
+    beforeEnter: () => {
+      const userData = localStorage.getItem("userData");
+      if (!userData) {
+        router.push("/config");
+      }
+    },
+  },
+  {
+    path: "/config",
+    name: "config",
+    component: ConfigPage,
   },
   {
     path: "/:catchAll(.*)*",
